00530e1a7d756c599958ec0b2ff5380f64400405
Jakov764 <55740028+Jakov764@users.noreply.github.com>
2020-10-13 10:46:02 +0200

    Add files via upload

---
+++ cvekŠpraljaJakovNasljeđivanje/cvekŠpraljaJakovNasljeđivanje/ListBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cvekŠpraljaJakovNasljeđivanje
+{
+    class ListBox : IControl
+    {
+        private int brojac;
+        public int Brojac
+        {
+            get { return brojac; }
+            set { brojac = value; }
+        }
+
+        public void Metoda()
+        {
+            Console.WriteLine("ListBox");
+
+        }
+    }
+}
---
+++ cvekŠpraljaJakovNasljeđivanje/cvekŠpraljaJakovNasljeđivanje/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cvekŠpraljaJakovNasljeđivanje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListBox x = new ListBox();
+            ComboBox y = new ComboBox();
+
+            x.Metoda();
+            y.Metoda();
+
+            Console.ReadKey();
+        }
+    }
+}
